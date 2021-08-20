e17310067e880da4972a9b4611676594e58af0a5
Cheong Myeong Noh <cjdauddl93@gmail.com>
2021-08-20 18:13:21 +0900

    second

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9ku!dvzac9d-jhiqwlji5vyi&8@w#-0v!riomtxhe8$8&#grjn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
